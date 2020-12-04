9698e04db61b6fef1d66b8d11226f4b24594a714
Joao Lambert <54284662+devlambertjoao@users.noreply.github.com>
2020-12-04 11:59:39 -0300

    Update postInstall.sh

--- postInstall.sh
+++ postInstall.sh
@@ -64,6 +64,7 @@ cd
 
 echo "Instalando ohmyzsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 echo "Configurando ruby on rails"
 gem install rails
