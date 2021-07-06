82391c12aa0488084690a9798320f695d2f408e4
Joao Lambert <54284662+devlambertjoao@users.noreply.github.com>
2021-07-06 17:29:00 -0300

    Add files via upload

---
+++ firacode-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd ~/Downloads
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+mkdir -p /usr/share/fonts/truetype/firacode
+sudo mv FiraCode /usr/share/fonts/truetype/firacode/FiraCode.zip
+cd /usr/share/fonts/truetype/firacode
+sudo unzip FiraCode.zip
+fc-cache -f -v
+cd
+
