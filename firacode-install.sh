50361efb48a6934fd642fa408273fe9dbded518b
Joao Lambert <54284662+devlambertjoao@users.noreply.github.com>
2021-07-07 21:25:30 +0100

    Update firacode-install.sh

--- firacode-install.sh
+++ firacode-install.sh
@@ -2,7 +2,7 @@
 cd ~/Downloads
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
 mkdir -p /usr/share/fonts/truetype/firacode
-sudo mv FiraCode /usr/share/fonts/truetype/firacode/FiraCode.zip
+sudo mv FiraCode.zip /usr/share/fonts/truetype/firacode/FiraCode.zip
 cd /usr/share/fonts/truetype/firacode
 sudo unzip FiraCode.zip
 fc-cache -f -v
