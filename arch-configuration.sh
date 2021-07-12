087dfe96a96a4d60366467b0d3ba869bb831bff7
Joao Lambert <54284662+devlambertjoao@users.noreply.github.com>
2021-07-12 10:36:04 -0300

    Update arch-configuration.sh

--- arch-configuration.sh
+++ arch-configuration.sh
@@ -1,20 +1,18 @@
 # For WSL "Run as root first"
-useradd -m -G wheel lambert
+# useradd -m -G wheel lambert
 # Adicionar em /etc/sudoers: lambert ALL=(ALL) ALL
-passwd lambert
+# passwd lambert
+# pacman-key --init
+# pacman-key --populate
+# pacman-key --refresh-keys
+# pacman -Sy archlinux-keyring
+# pacman -Syyu
 
-pacman-key --init
-pacman-key --populate
-pacman-key --refresh-keys
-pacman -Sy archlinux-keyring
-pacman -Syyu
+# Att packages
+sudo pacman -Syyu
 
-# Alacritty
-sudo pacman -S alacritty
-mkdir -p .config/alacritty
-cd .config/alacritty
-wget https://raw.githubusercontent.com/devlambertjoao/dotfiles/master/alacritty.yml
-cd
+# Base Devel
+sudo pacman -S base-devel
 
 # Zsh
 # sudo apt install zsh
@@ -23,6 +21,13 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 wget https://raw.githubusercontent.com/devlambertjoao/dotfiles/master/.zshrc
 sudo mv .zshrc.1 .zshrc
+
+# Alacritty
+sudo pacman -S alacritty
+mkdir -p ~/.config/alacritty
+cd ~/.config/alacritty
+wget https://raw.githubusercontent.com/devlambertjoao/dotfiles/master/alacritty.yml
+cd
 
 # Tmux
 sudo pacman -S tmux
@@ -46,9 +51,10 @@ asdf global java openjdk-15
 sudo pacman -S maven
 
 # JDK from oracle
-sudo mv jdk-11.0.11_linux-x64_bin.tar.gz /usr/lib/jvm
-cd /usr/lib/jvm 
+sudo mv ~/Downloads/jdk-11.0.11_linux-x64_bin.tar.gz /usr/lib/jvm/jdk-11.0.11_linux-x64_bin.tar.gz
+cd /usr/lib/jvm
 sudo tar -zxvf jdk-11.0.11_linux-x64_bin.tar.gz
+cd ~
 
 # Nodejs
 asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
@@ -76,8 +82,8 @@ sudo pacman -S python python-pip
 # Neovim
 # sudo apt install neovim silversearcher-ag ctags
 sudo pacman -S neovim the_silver_searcher ctags
-mkdir -p .config/nvim
-cd .config/nvim
+mkdir -p ~/.config/nvim
+cd ~/.config/nvim
 wget https://raw.githubusercontent.com/devlambertjoao/dotfiles/master/init.vim
 wget https://projectlombok.org/downloads/lombok.jar
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
@@ -97,35 +103,35 @@ nvim
 
 # Neovide
 sudo pacman -S rust llvm-libs
-cd Downloads
-https://aur.archlinux.org/neovide-git.git
+cd ~/Downloads
+git clone https://aur.archlinux.org/neovide-git.git
+cd neovide-git
 makepkg --syncdeps
+sudo pacman -U neovide-git-*-x86_64.pkg.tar.zst
 
 # STS
-mkdir Programs
-cd Programs
-wget https://download.springsource.com/release/STS4/4.9.0.RELEASE/dist/e4.18/spring-tool-suite-4-4.9.0.RELEASE-e4.18.0-linux.gtk.x86_64.tar.gz
-tar -zxvf spring-tool-suite-4-4.9.0.RELEASE-e4.18.0-linux.gtk.x86_64.tar.gz
-cd sts-4.9.0.RELEASE
+mkdir ~/Programs
+cd ~/Programs
+wget https://download.springsource.com/release/STS4/4.11.0.RELEASE/dist/e4.20/spring-tool-suite-4-4.11.0.RELEASE-e4.20.0-linux.gtk.x86_64.tar.gz
+tar -zxvf spring-tool-suite-4-*.x86_64.tar.gz
+cd sts-*.RELEASE
 wget https://projectlombok.org/downloads/lombok.jar
-sudo echo "~/Programs/sts-4.9.0.RELEASE/SpringToolSuite4" > /usr/bin/sts4
-sudo chmod + /usr/bin/sts
-cd /usr/share/applications
-sudo wget https://raw.githubusercontent.com/devlambertjoao/dotfiles/master/sts.desktop
+sudo echo "~/Programs/sts-4.11.0.RELEASE/SpringToolSuite4" > sts
+sudo chmod +x sts
+sudo mv sts /usr/bin/sts
 
 # Eclipse
-cd Programs
-wget https://eclipse.c3sl.ufpr.br/technology/epp/downloads/release/2021-03/R/eclipse-jee-2021-03-R-linux-gtk-x86_64.tar.gz
-tar -zxvf eclipse-jee-2021-03-R-linux-gtk-x86_64.tar.gz
+cd ~/Programs
+wget https://eclipse.c3sl.ufpr.br/technology/epp/downloads/release/2021-06/R/eclipse-jee-2021-06-R-linux-gtk-x86_64.tar.gz
+tar -zxvf eclipse-jee-*-x86_64.tar.gz
 cd eclipse
 wget https://projectlombok.org/downloads/lombok.jar
-sudo echo "~/Programs/eclipse/eclipse" > /usr/bin/eclipse
-sudo chmod + /usr/bin/eclipse
-cd /usr/share/applications
-sudo wget https://raw.githubusercontent.com/devlambertjoao/dotfiles/master/eclipse.desktop
+sudo echo "~/Programs/eclipse/eclipse" > eclipse
+sudo chmod +x eclipse
+sudo mv eclipse /usr/bin/eclipse
 
 # Android Studio
-cd Downloads
+cd ~/Downloads
 git clone https://aur.archlinux.org/android-studio.git
 cd android-studio
 makepkg --syncdeps
@@ -139,16 +145,16 @@ $ANDROID_HOME/tools/bin/sdkmanager --licenses
 source .zshrc
 
 # VS Code
-cd Downloads
+cd ~/Downloads
 git clone https://aur.archlinux.org/visual-studio-code-bin.git
 cd visual-studio-code-bin
 makepkg --syncdeps
 sudo pacman -U visual-studio-code-bin-*.pkg.tar.zst
 
 # Docker
-sudo pacman -S docker
-sudo systemctl enable docker.service
-sudo systemctl start docker.service
+sudo pacman -S docker &
+sudo systemctl enable docker.service &
+sudo systemctl start docker.service &
 sudo usermod -aG docker lambert
 
 # Run SQL on docker for local development:
@@ -158,14 +164,14 @@ docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=password@1" -p 1433:1433 --name sq
 sudo pacman -S dbeaver
 
 # MongoDB Compass
-cd Downloads
+cd ~/Downloads
 git clone https://aur.archlinux.org/mongodb-compass.git
 cd mongodb-compass
 makepkg --syncdeps
 sudo pacman -U mongodb-compass-*-x86_64.pkg.tar.zst
 
 # Spotify
-cd Downloads
+cd ~/Downloads
 git clone https://aur.archlinux.org/spotify.git
 cd spotify
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | gpg --import -
