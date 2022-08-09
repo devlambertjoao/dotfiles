05e500f5b56560c1f4c35a70b604782b97b3e4a9
Joao Lambert <54284662+devlambertjoao@users.noreply.github.com>
2022-08-09 15:33:06 -0300

    Update arch-configuration.sh

--- arch-configuration.sh
+++ arch-configuration.sh
@@ -1,13 +1,16 @@
+###################################################### START WSL SETUP ######################################################
 # For WSL "Run as root first"
-# useradd -m -G wheel lambert
-# Adicionar em /etc/sudoers: lambert ALL=(ALL) ALL
-# passwd lambert
-# pacman-key --init
-# pacman-key --populate
-# pacman-key --refresh-keys
-# pacman -Sy archlinux-keyring
-# pacman -Syyu
+useradd -m -G wheel lambert
+# Add in /etc/sudoers: lambert ALL=(ALL) ALL
+passwd lambert
+pacman-key --init
+pacman-key --populate
+pacman-key --refresh-keys
+pacman -Sy archlinux-keyring
+pacman -Syyu
+###################################################### END WSL SETUP ######################################################
 
+###################################################### START BASIC SETUP ######################################################
 # Att packages
 sudo pacman -Syyu
 
@@ -19,9 +22,9 @@ ssh-keygen -o -t rsa -C “a@example.com”
 
 # Base Devel
 sudo pacman -S base-devel
+###################################################### END BASIC SETUP ######################################################
 
-# Zsh
-# sudo apt install zsh
+###################################################### START ZSH ######################################################
 sudo pacman -S zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -29,28 +32,33 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 wget https://raw.githubusercontent.com/kyoz/purify/master/zsh/purify.zsh-theme -O ~/.oh-my-zsh/themes/purify.zsh-theme
 wget https://raw.githubusercontent.com/devlambertjoao/dotfiles/master/.zshrc
 sudo mv .zshrc.1 .zshrc
+###################################################### END ZSH ######################################################
 
-# Tmux
+###################################################### START Tmux ######################################################
 sudo pacman -S tmux
 wget https://raw.githubusercontent.com/devlambertjoao/dotfiles/master/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+###################################################### END Tmux ######################################################
 
-# Asdf
+###################################################### START Asdf ######################################################
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0
 echo ". $HOME/.asdf/asdf.sh" >> .bashrc
 echo ". $HOME/.asdf/completions/asdf.bash" >> .bashrc
 source .bashrc
 source .zshrc
+###################################################### END Asdf ######################################################
 
+###################################################### Start Rust ######################################################
+asdf plugin-add rust https://github.com/code-lever/asdf-rust.git
+asdf install rust stable
+asdf global rust stable
+###################################################### END Rust ######################################################
+
+###################################################### START Java, Maven ######################################################
 # Java
 asdf plugin-add java https://github.com/halcyon/asdf-java.git
 asdf install java openjdk-15
 asdf global java openjdk-15
-
-# Rust
-asdf plugin-add rust https://github.com/code-lever/asdf-rust.git
-asdf install rust stable
-asdf global rust stable
 
 # JDK from oracle
 cd Downloads
@@ -64,7 +72,9 @@ cd
 # Maven
 # sudo apt install maven
 sudo pacman -S maven
+###################################################### END Java, Maven ######################################################
 
+###################################################### START Node, Npm, Yarn ######################################################
 # Nodejs
 asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf install nodejs 16.15.0
@@ -78,8 +88,9 @@ sudo pacman -S yarn
 # Npm
 mkdir ~/.npm-global
 npm config set prefix '~/.npm-global'
+###################################################### END Node, Npm, Yarn ######################################################
 
-# Ruby
+###################################################### START Ruby ######################################################
 # If inside WSL: sudo pacman -S base-devel
 asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
 echo rails > .default-gems
@@ -88,15 +99,18 @@ echo bundler >> .default-gems
 echo rsense >> .default-gems
 asdf install ruby 3.1.2
 asdf global ruby 3.1.2
+###################################################### END RUBY ######################################################
 
-# Python
+###################################################### START Python ######################################################
 sudo pacman -S python python-pip
+###################################################### END Python ######################################################
 
-# .NET 
+###################################################### START .NET ######################################################
 sudo pacman -S dotnet-sdk aspnet-runtime
 dotnet tool install --global csharp-ls
+###################################################### END .NET ######################################################
 
-# Neovim
+###################################################### START Neovim ######################################################
 # sudo apt install neovim silversearcher-ag ctags
 sudo pacman -S neovim the_silver_searcher ctags
 pip install neovim
@@ -106,9 +120,8 @@ wget https://raw.githubusercontent.com/devlambertjoao/dotfiles/master/.config/nv
 wget https://projectlombok.org/downloads/lombok.jar
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-## LSP Deps
-# Tsserver
-npm install -g typescript typescript-language-server
+
+################## LSP Deps
 # JDTLS (Java)
 cd ~/Downloads
 wget https://download.eclipse.org/jdtls/snapshots/jdt-language-server-1.12.0-202205122107.tar.gz
@@ -118,26 +131,32 @@ cd ~/Programs/jdtls
 tar -zxvf jdt-language-server-1.12.0-202205122107.tar.gz
 rm jdt-language-server-1.12.0-202205122107.tar.gz
 cd
-# HTML and CSS (CSSLS)
-npm i -g vscode-langservers-extracted eslint eslint_d
+
+# HTML, CSS (CSSLS), Typescript
+npm i -g typescript typescript-language-server vscode-langservers-extracted eslint eslint_d
+
 # Omnisharp (C#)
 cd ~/Downloads
-# For Mac
-# wget https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.39.0/omnisharp-osx.tar.gz
-# For Archlinux
+## For Mac
+wget https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.39.0/omnisharp-osx.tar.gz
+
+## For Archlinux
 sudo pacman -S mono mono-msbuild
-wget https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.39.0/omnisharp-linux-x64.tar.gz
-mkdir -p ~/Programs/omnisharp
-mv omnisharp-linux-x64.tar.gz ~/Programs/omnisharp
-cd ~/Programs/omnisharp
-tar -zxvf omnisharp-linux-x64.tar.gz
-rm omnisharp-linux-x64.tar.gz
+wget https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.39.0/omnisharp-linux-x64-net6.0.tar.gz
+mkdir -p ~/Programs/omnisharp-net6.0
+mv omnisharp-linux-x64-net6.0.tar.gz ~/Programs/omnisharp-net6.0
+cd ~/Programs/omnisharp-net6.0
+tar -zxvf omnisharp-linux-x64-net6.0.tar.gz
+rm omnisharp-linux-x64-net6.0.tar.gz
 cd
+
 # Pyright
 npm install -g pyright
-# run :PlugInstall on neovim
 
-# STS
+### Remember to run :PlugInstall on neovim
+###################################################### END Neovim ######################################################
+
+###################################################### START STS ######################################################
 mkdir ~/Programs
 cd ~/Programs
 wget https://download.springsource.com/release/STS4/4.11.0.RELEASE/dist/e4.20/spring-tool-suite-4-4.11.0.RELEASE-e4.20.0-linux.gtk.x86_64.tar.gz
@@ -148,8 +167,9 @@ wget https://projectlombok.org/downloads/lombok.jar
 sudo echo "~/Programs/sts-4.11.0.RELEASE/SpringToolSuite4" > sts
 sudo chmod +x sts
 sudo mv sts /usr/bin/sts
+###################################################### END STS ######################################################
 
-# Eclipse
+###################################################### START Eclipse ######################################################
 cd ~/Programs
 wget https://eclipse.c3sl.ufpr.br/technology/epp/downloads/release/2021-06/R/eclipse-jee-2021-06-R-linux-gtk-x86_64.tar.gz
 tar -zxvf eclipse-jee-*-x86_64.tar.gz
@@ -159,8 +179,9 @@ wget https://projectlombok.org/downloads/lombok.jar
 sudo echo "~/Programs/eclipse/eclipse" > eclipse-starter
 sudo chmod +x eclipse-starter
 sudo mv eclipse-starter /usr/bin/eclipse
+###################################################### END Eclipse ######################################################
 
-# Android Studio
+###################################################### START Android Studio ######################################################
 cd ~/Downloads
 git clone https://aur.archlinux.org/android-studio.git
 cd android-studio
@@ -177,15 +198,17 @@ source ~/.zshrc
 echo "~/Programs/Android/Sdk/emulator/emulator -avd RN-Device" > rndevice
 sudo chmod +x rndevice
 sudo mv rndevice /usr/bin/rndevice
+###################################################### END Android Studio ######################################################
 
-# VS Code
+###################################################### START VS Code ######################################################
 cd ~/Downloads
 git clone https://aur.archlinux.org/visual-studio-code-bin.git
 cd visual-studio-code-bin
 makepkg --syncdeps
 sudo pacman -U visual-studio-code-bin-*.pkg.tar.zst
+###################################################### END VS Code ######################################################
 
-# Docker
+###################################################### START Docker ######################################################
 sudo pacman -S docker
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
@@ -197,20 +220,37 @@ docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=password@1" -p 1433:1433 --name sq
 # For mac
 # docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=password@1' --name sql -p 1433:1433 --restart always -d mcr.microsoft.com/azure-sql-edge:latest
 
-# Dbeaver
-sudo pacman -S dbeaver
+###################################################### END Docker ######################################################
 
-# MongoDB Compass
+###################################################### START Dbeaver ######################################################
+sudo pacman -S dbeaver
+###################################################### END Dbeaver ######################################################
+
+###################################################### START MongoDB Compass ######################################################
 cd ~/Downloads
 git clone https://aur.archlinux.org/mongodb-compass.git
 cd mongodb-compass
 makepkg --syncdeps
 sudo pacman -U mongodb-compass-*-x86_64.pkg.tar.zst
+###################################################### END MongoDB Compass ######################################################
 
-# Spotify
+###################################################### START Spotify ######################################################
 cd ~/Downloads
 git clone https://aur.archlinux.org/spotify.git
 cd spotify
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | gpg --import -
 makepkg --syncdeps
 sudo pacman -U spotify-*-x86_64.pkg.tar.zst
+###################################################### END Spotify ######################################################
+
+
+###################################################### BACKUPS OF OLDER APPS ######################################################
+######### Backup older versions
+## wget https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.39.0/omnisharp-linux-x64.tar.gz
+## mkdir -p ~/Programs/omnisharp
+## mv omnisharp-linux-x64.tar.gz ~/Programs/omnisharp
+## cd ~/Programs/omnisharp
+## tar -zxvf omnisharp-linux-x64.tar.gz
+## rm omnisharp-linux-x64.tar.gz
+## cd
+######### Backup older versions
