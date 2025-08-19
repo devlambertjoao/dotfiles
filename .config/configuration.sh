a5101688815783ad35ce498589e9d34e26d01d88
Joao Lambert <54284662+devlambertjoao@users.noreply.github.com>
2025-08-19 18:52:45 -0300

    Update configuration.sh

--- .config/configuration.sh
+++ .config/configuration.sh
@@ -1,3 +1,6 @@
+# For arch linux only
+sudo pacman -S base-devel libyaml wget git zsh 
+
 # Installing Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
